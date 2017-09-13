61bcd939b2ff7e2b5de8e7969d4c4653c858f043
Ailleen <wangmaomao_by@126.com>
2017-09-13 17:15:41 +0800

    添加lombok

--- src/main/java/com/springmvc/model/BlogInfo.java
+++ src/main/java/com/springmvc/model/BlogInfo.java
@@ -1,11 +1,15 @@
 package com.springmvc.model;
 
+import lombok.Data;
+
 /**
  * Created by  Aileen on 2017/9/12.
  */
+@Data
 public class BlogInfo {
-
-
     private String id;
-
+    private String creatTime; //博客的创作时间
+    private String author; //作者
+    private String title; //博客标题
+    private String blogContext; //博客内容
 }
